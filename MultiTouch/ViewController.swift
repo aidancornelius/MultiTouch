c1b8217cd5482e66ce20c53a1b77a0004516939b
Aidan Cornelius-Bell <ac@schemist.com.au>
2023-06-12 07:16:53 +0930

    Initial Commit

---
+++ MultiTouch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MultiTouch
+//
+//  Created by Aidan Cornelius-Bell on 12/6/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
